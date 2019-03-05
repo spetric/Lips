7468f6ae139555ea6aa356747ef7e36fd2178ad9
spetric <47104720+spetric@users.noreply.github.com>
2019-03-05 20:52:27 +0100

    Add files via upload

---
+++ hostApp/projLips.cpp
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include <tchar.h>
+//---------------------------------------------------------------------------
+#include <Vcl.Styles.hpp>
+#include <Vcl.Themes.hpp>
+USEFORM("spLipsHost\frames\FrameListBoxU.cpp", FrameListBox); /* TFrame: File Type */
+USEFORM("spLipsHost\frames\FrameImageU.cpp", FrameImage); /* TFrame: File Type */
+USEFORM("spLipsHost\frames\FrameImageSliderU.cpp", FrameImageSlider); /* TFrame: File Type */
+USEFORM("spLipsHost\frmLipsU.cpp", frmLips);
+USEFORM("spLipsHost\frames\FrameSliderU.cpp", FrameSlider); /* TFrame: File Type */
+USEFORM("spLipsHost\frames\FrameImageComboU.cpp", FrameImageCombo); /* TFrame: File Type */
+USEFORM("spLipsHost\frames\FrameComboBoxU.cpp", FrameComboBox); /* TFrame: File Type */
+USEFORM("spLipsHost\frames\FrameCheckBoxU.cpp", FrameCheckBox); /* TFrame: File Type */
+//---------------------------------------------------------------------------
+int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+{
+	try
+	{
+		Application->Initialize();
+		Application->MainFormOnTaskBar = true;
+		TStyleManager::TrySetStyle("Charcoal Dark Slate");
+		Application->CreateForm(__classid(TfrmLips), &frmLips);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
